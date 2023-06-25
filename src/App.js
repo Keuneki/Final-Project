8ea6d3b0d600f0d7a03abba65e794e3af74abba9
Ministry of Feeding <130564319+Keuneki@users.noreply.github.com>
2023-06-25 19:11:26 +0300

    Crash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AllElements from './pages/AllElements';
 import SingleElement from './pages/SingleElement';
 import AllMagic from './pages/AllMagic';
 import SingleMagic from './pages/SingleMagic';
 import PureMagic from './pages/PureMagic';
 import MixedMagic from './pages/MixedMagic';
-import MagicBuilder from './components/MagicBuilder';
+import MagicBuilder from './pages/MagicBuilder';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={AllElements} />
-        <Route path="/element/:id" component={SingleElement} />
-        <Route exact path="/magic-builder" component={MagicBuilder} />
-        <Route exact path="/magic" component={AllMagic} />
-        <Route path="/magic/:id" component={SingleMagic} />
-        <Route path="/pure-magic" component={PureMagic} />
-        <Route path="/mixed-magic" component={MixedMagic} />
-      </Switch>
+      <Route exact path="/" component={AllElements} />
+      <Route path="/element/:id" component={SingleElement} />
+      <Route exact path="/magic-builder" component={MagicBuilder} />
+      <Route exact path="/magic" component={AllMagic} />
+      <Route path="/magic/:id" component={SingleMagic} />
+      <Route path="/pure-magic" component={PureMagic} />
+      <Route path="/mixed-magic" component={MixedMagic} />
     </Router>
   );
 };
